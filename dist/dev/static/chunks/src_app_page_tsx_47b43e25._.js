398e70db71d269c9683b47caa13caa70767226bb
ganeshlucky07 <ganeshlucky07@gmail.com>
2026-03-03 23:22:35 +0530

    Remove dist folder and add to .gitignore

--- dist/dev/static/chunks/src_app_page_tsx_47b43e25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_092dae47._.js",
-  "static/chunks/node_modules_1eef84ea._.js"
-],
-    source: "dynamic"
-});
--- dist/dev/static/chunks/src_app_page_tsx_6a5e0844._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_e654292e._.js",
-  "static/chunks/node_modules_355cbba7._.js"
-],
-    source: "dynamic"
-});
